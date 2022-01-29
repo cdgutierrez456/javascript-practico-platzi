7612f3d5d96457719aa837befe84652412a0a9b3
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-01-28 22:24:37 -0500

    Haciendo operaciones con salarios de lista de personas

---
+++ Salarios/salarios.js
@@ -0,0 +1,46 @@
+const colombia = [];
+
+colombia.push({
+    name: "Cristian",
+    salary: 500
+})
+
+colombia.push({
+    name: "Daniela",
+    salary: 1500
+})
+
+colombia.push({
+    name: "Sofia",
+    salary: 2000
+})
+
+colombia.push({
+    name: "Mateo",
+    salary: 3500
+})
+
+colombia.push({
+    name: "Andres",
+    salary: 1500
+})
+
+colombia.push({
+    name: "Antonia",
+    salary: 4500
+})
+
+colombia.push({
+    name: "Erika",
+    salary: 500
+})
+
+colombia.push({
+    name: "Jennifer",
+    salary: 2000
+})
+
+colombia.push({
+    name: "Sergio",
+    salary: 5500
+})
